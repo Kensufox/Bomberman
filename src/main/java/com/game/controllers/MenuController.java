6b7c53ae86c41503efdf91fdce89ab929c551bdc
Loan <loan.allard@etu.univ-amu.fr>
2025-06-06 09:43:57 +0200

    Ajout Menu Choix Jeux, Mise en page des menu, implémentations des fonctionnalités des boutons...

--- src/main/java/com/game/controllers/MenuController.java
+++ src/main/java/com/game/controllers/MenuController.java
@@ -4,18 +4,25 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 
 public class MenuController {
 
+    // Menu Principal
     @FXML private Button jouer;
     @FXML private Button options;
     @FXML private Button quitter;
 
+    // Menu Jouer
+    @FXML private Button btnClassic;
+    @FXML private Button btnCaptureTheFlag;
+    @FXML private Button btnContreLOrdi;
+
     @FXML
     public void jouer(ActionEvent event) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/game_map.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/choose_game.fxml"));
             StackPane root = loader.load();
 
             // Retrieves the current scene from one of the buttons
@@ -33,6 +40,37 @@ public class MenuController {
     @FXML
     public void exit () {
         System.exit(0);
+    }
+
+    // MENU JOUER
+    @FXML
+    public void classicGame (ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/game_map.fxml"));
+            StackPane root = loader.load();
+
+            // Retrieves the current scene from one of the buttons
+            Button sourceButton = (Button) event.getSource();
+            sourceButton.getScene().setRoot(root);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void retourMenu (ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/main_menu.fxml"));
+            AnchorPane root = loader.load();
+
+            // Retrieves the current scene from one of the buttons
+            Button sourceButton = (Button) event.getSource();
+            sourceButton.getScene().setRoot(root);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
